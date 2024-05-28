106d83232d85153cdbc9123590f0e6d0c6f5d8ff
viraljain7 <veerljain1234@gmail.com>
2024-05-28 14:44:59 +0530

    Comments added to for understanding

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,28 +1,38 @@
-
+// The ApiError class extends the built-in Error class to represent errors in API responses.
 class ApiError extends Error {
 
-    constructor(statusCode,
+
+    /**
+     * Constructor for the ApiError class.
+     * @param {number} statusCode - The HTTP status code for the error response.
+     * @param {string} [message="Something Went Wrong"] - A message describing the error. Defaults to "Something Went Wrong".
+     * @param {Array} [errors=[]] - An array of specific error details. Defaults to an empty array.
+     * @param {string} [stack=""] - The stack trace for the error. Defaults to an empty string.
+     */
+
+
+    constructor(
+        statusCode,
         message = "Something Went Wrong",
         errors = [],
         stack = ""
     ) {
 
-        super(message);
-        this.statusCode = statusCode;
-        this.errors = errors;
-        this.data = null;
-        this.message = message;
-        this.success = false;
+        super(message);  // Calls the constructor of the parent Error class with the error message.
 
+        this.statusCode = statusCode;  // The HTTP status code of the error response.
+        this.errors = errors;          // An array of specific error details.
+        this.data = null;              // No data is associated with an error response.
+        this.message = message;        // The error message.
+        this.success = false;          // Indicates that the response is not successful.
+
+        // If a stack trace is provided, set it. Otherwise, capture the current stack trace.
         if (stack) {
-            this.stack = stack
+            this.stack = stack;
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor);
         }
-
-
     }
-
 }
 
 export { ApiError };
--- src/utils/ApiResopnse.js
+++ src/utils/ApiResopnse.js
@@ -1,8 +1,15 @@
+// The ApiResponse class is used to standardize the structure of API responses.
 class ApiResponse {
+    /**
+     * Constructor for the ApiResponse class.
+     * @param {number} statusCode - The HTTP status code for the response.
+     * @param {string} [message="success"] - A message describing the response. Defaults to "success".
+     * @param {any} data - The data to be included in the response.
+     */
     constructor(statusCode, message = "success", data) {
-        this.statusCode = statusCode;
-        this.data = data;
-        this.message = message;
-        this.success = statusCode < 400
+        this.statusCode = statusCode;  // The HTTP status code of the response.
+        this.data = data;              // The data to be included in the response.
+        this.message = message;        // A message describing the response, defaulting to "success".
+        this.success = statusCode < 400; // A boolean indicating if the response is successful (status codes below 400 are considered successful).
     }
 }
